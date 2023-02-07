69c327a1a7c23aad17b6482dcde0f158def86907
Mert Can Altın <mertgold60@gmail.com>
2023-02-07 18:23:39 +0300

    added tv

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,53 @@ export default function Home() {
   return (
     <>
       <div class="grid-cols-3 gap-4  md:grid grid-cols-4 gap-4">
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/XEJM4Hcgd3M?autoplay=1&mute=1" title="NTV" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/SqHIO2zhxbA?autoplay=1&mute=1" title="Habertürk" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/99Qfz4jxfpI?autoplay=1&mute=1" title="Haber Global" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/Rc5qrxlJZzc?autoplay=1&mute=1" title="TRT Haber" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/sd94keSra6A?autoplay=1&mute=1" title="TV 100" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/L0aI7O5KrVU?autoplay=1&mute=1" title="Halk TV" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/TPbdeNMaAZY?autoplay=1&mute=1" title="24 TV" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/8YPC2IV7ve0?autoplay=1&mute=1" title="TGRT Haber" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
-        <div>
+        <div >
           <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/3QDiWPZ2D_k?autoplay=1&mute=1" title="KRT TV" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        </div>
+        <div >
+          <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/mRK3wXGdsLk?autoplay=1&mute=1" title="TELE 1" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        </div>
+        <div >
+          <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/7su_1By-cBk?autoplay=1&mute=1" title="Bengü Türk" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        </div>
+        <div >
+          <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/hHSmBJk6w0c?autoplay=1&mute=1" title="Bloomberg HT" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        </div>
+        <div >
+          <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/SdCJquYL-CQ?autoplay=1&mute=1" title="Ulusal Kanal" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        </div>
+        <div >
+          <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/xpoetRCJKqY?autoplay=1&mute=1" title="Artı TV" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        </div>
+        <div >
+          <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/SR396EBvGUk?autoplay=1&mute=1" title="TVNET" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+        </div>
+        <div >
+          <iframe  width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/1-Ng6S1dhNI?autoplay=1&mute=1" title="Ülke TV" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
         </div>
       </div>
     </>
